eb164d7788cadcd1eb9e5b988a96125a70257ce4
Pedro Da Silva Vitorino <114833041+pedrovitorino1@users.noreply.github.com>
2025-10-29 20:37:41 -0300

    Add renderProject function to display projects

--- js/templates.js
+++ js/templates.js
@@ -1,1 +1,19 @@
+ function renderProject(project) {
+  const container = document.getElementById('projectsContainer');
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = `
+    <h3>${project.title}</h3>
+    <p>${project.description}</p>
+    <button>Saiba Mais</button>
+  `;
+  container.appendChild(card);
+}
+
+ const projects = [
+  {title: 'Projeto A', description: 'Descrição do projeto A'},
+  {title: 'Projeto B', description: 'Descrição do projeto B'}
+];
+
+projects.forEach(p => renderProject(p));
 
